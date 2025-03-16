cbccd43a6e16c4e1118603463570b777546e2ab4
lenayoo <lenayoo17@gmail.com>
2025-03-16 23:14:25 +0900

    add inspirational img

--- frontend/src/components/Inspiration.tsx
+++ frontend/src/components/Inspiration.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import './style.css';
 
 export const Inspiration = () => {
   return (
     <div className='insp_main'>
       <div className='insp_header'>find your today's inspiraltional quotes</div>
-      <div className='insp_box'>click to find out</div>
+      <div className='insp_box'>
+        <img src='insp_cloud.gif' />
+      </div>
       <div className='insp_result'>result</div>
     </div>
   );
