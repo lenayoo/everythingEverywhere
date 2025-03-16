e82005ad2cbc2ac5922c5ccb702fccc317475592
lenayoo <lenayoo17@gmail.com>
2025-03-16 11:27:51 +0900

    add new feature of inspiration

--- frontend/src/components/Inspiration.tsx
+++ frontend/src/components/Inspiration.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import './style.css';
 
 export const Inspiration = () => {
-  return <div>Inspiration</div>;
+  return (
+    <div className='insp_main'>
+      <div className='insp_header'>find your today's inspiraltional quotes</div>
+      <div className='insp_box'>click to find out</div>
+      <div className='insp_result'>result</div>
+    </div>
+  );
 };
