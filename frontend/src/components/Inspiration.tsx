bff93333e916c375870db3df3240e8358c6e787d
lenayoo <lenayoo17@gmail.com>
2025-03-18 22:43:25 +0900

    complete inspirational quote

--- frontend/src/components/Inspiration.tsx
+++ frontend/src/components/Inspiration.tsx
@@ -1,13 +1,34 @@
+import { useState } from 'react';
 import './style.css';
+import rumiQuotes from '../apis/rumi_quotes.json';
+
+type Quote = string | null;
 
 export const Inspiration = () => {
+  const [quote, setQuote] = useState<Quote>(null);
+
+  const randomInspirationalQuotes = () => {
+    const randomNum = Math.floor(Math.random() * 82) + 1;
+    const newQuote = rumiQuotes.quotes[randomNum].text;
+    console.log(newQuote);
+    setQuote(newQuote);
+  };
+
   return (
     <div className='insp_main'>
-      <div className='insp_header'>find your today's inspiraltional quotes</div>
-      <div className='insp_box'>
+      <div className='insp_header'>
+        <h2>Find today's Rumi word</h2>
+      </div>
+      <div className='insp_box' onClick={randomInspirationalQuotes}>
         <img src='insp_cloud.gif' />
       </div>
-      <div className='insp_result'>result</div>
+      {quote ? (
+        <div className='insp_result'>
+          <p>{quote}</p>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
